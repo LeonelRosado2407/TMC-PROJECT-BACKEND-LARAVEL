b93aecf120ad045b7692884ce85cf507a6025dd6
AdolfoVaz <146690208+AdolfoVaz@users.noreply.github.com>
2024-02-01 11:08:48 -0600

    add: sidevar

--- resources/js/components/SidebarComponent.vue
+++ resources/js/components/SidebarComponent.vue
@@ -1,12 +1,11 @@
 <template>
-<aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-        <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+    <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
+        <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
             <ul class="space-y-2 font-medium">
                 <slot></slot>
             </ul>
         </div>
-</aside>
-
+        </aside>
 </template>
 
 <script>
--- resources/views/Pruebas/pruebas.blade.php
+++ resources/views/Pruebas/pruebas.blade.php
@@ -1,10 +1,17 @@
-@extends('layouts.app')
+@extends('layouts.admin')
  
 @section('content')
-
-
-
-
-
+<table-component>
+    <table-item
+    columna="iD"
+    dato="122"
+    >
+    </table-item>
+    <table-item
+    columna="iD"
+    dato="122"
+    >
+    </table-item>
+</table-component>
 
 @endsection
